74ee1ba3ad123a522fc635011be3228fa8c537c1
Dora <939217129@qq.com>
2021-11-22 15:44:19 +0800

    准备上传

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-console.log('hi')
-console.log('ho')
+import './style.css'
+import './style2.scss'
+import './style3.less'
+import './style4.styl'
+
+const app = document.getElementById('app')
+app.innerHTML = `<img src="${png}">`
+
+import png from './assets/girl.jpg'
+
+// 当点击按钮时，我再去加载某个js
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = ()=>{
+    const promise = import('./lazy') // 拿到的是一个延迟对象，promise,可以来监听成功失败
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    },()=>{})
+}
+app.appendChild(button)
+// 懒加载：用import()去加载这个文件，然后我会得到一个promise,promise.then前面写成功做什么，后面写失败做什么
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const base = require('./webpack.config.base.js') 
+
 module.exports = {
+    ...base, //意思是把base里面所有的属性放过来
     mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: '[name].[contenthash].js' 
-    }
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: "./dist"
+    },
+    module: {  
+      rules: [
+        ...base.module.rules,
+      {
+        test: /\.css$/i, 
+        use: ["style-loader", "css-loader"]
+        
+      }],
+    },
+    
   };
+
+
+  
+  
+  
