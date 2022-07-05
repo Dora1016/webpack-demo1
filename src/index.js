c8778c0c817733c72d705f658cc737da6bf6c2c8
Dora <939217129@qq.com>
2022-07-05 11:09:15 +0800

    修改图片大小

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './style3.less'
 import './style4.styl'
 
 const app = document.getElementById('app')
-app.innerHTML = `<img src="${png}">`
+app.innerHTML = `<img src="${png}"style="width:100%;height:90vh" >`
 
 import png from './assets/girl.jpg'
 
 // 当点击按钮时，我再去加载某个js
 const button = document.createElement('button')
-button.innerText = '懒加载'
+button.innerText = '懒加载  在控制台打印lazy.js内容'
 button.onclick = ()=>{
     const promise = import('./lazy') // 拿到的是一个延迟对象，promise,可以来监听成功失败
     promise.then((module)=>{
